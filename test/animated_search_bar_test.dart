bb3745700f092ea7d7204e2f3bec17f71a884c26
ukieTux <ukie.tux@gmail.com>
2020-09-22 11:34:35 +0800

    Add widget test

--- test/animated_search_bar_test.dart
+++ test/animated_search_bar_test.dart
@@ -1,10 +1,16 @@
+import 'package:animated_search_bar/animated_search_bar.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-
   TestWidgetsFlutterBinding.ensureInitialized();
 
-  /*test('getPlatformVersion', () async {
-    expect(await FlutterSearchBar.platformVersion, '42');
-  });*/
+  const MaterialApp app = MaterialApp(
+    home: Scaffold(body: const AnimatedSearchBar()),
+  );
+
+  testWidgets('app should work', (tester) async {
+    await tester.pumpWidget(app);
+    expect(find.byType(AnimatedSearchBar), findsOneWidget);
+  });
 }
