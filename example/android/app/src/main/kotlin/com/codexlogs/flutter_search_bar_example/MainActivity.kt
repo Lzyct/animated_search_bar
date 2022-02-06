0c71cd5b6914b4397f88886a0ee79e7d8ebd1e63
Lzyct <hey.mudassir@gmail.com>
2022-02-06 20:43:32 +0800

    Update example project configuration for Android

--- example/android/app/src/main/kotlin/com/codexlogs/flutter_search_bar_example/MainActivity.kt
+++ example/android/app/src/main/kotlin/com/codexlogs/flutter_search_bar_example/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.codexlogs.flutter_search_bar_example
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterActivity;
 
 class MainActivity: FlutterActivity() {
 }
